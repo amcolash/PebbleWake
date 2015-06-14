eb07725d78a5b15f7185f7c6f74f70416e60e07b
Andrew McOlash <amcolash@gmail.com>
2015-06-14 12:54:58 -0500

    Imlpemented basic interface

--- src/app.js
+++ src/app.js
@@ -5,56 +5,57 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var url = "pi.amcolash.com";
+var port = 4000;
+
+var computers = [
+  {
+    title: 'Andrew PC',
+    mac: 'FC:AA:14:96:01:E7',
+    ip: 'pi.amcolash.com',
+    port: '9'
+  }
+];
+
+var menu = new UI.Menu({
+  sections: [{
+    items: computers
+  }]
 });
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
+menu.on('select', function(e) {
+  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+  console.log('The item is titled "' + e.item.title + '"');
+  
+  var computer = computers[e.itemIndex];
+  console.log(JSON.stringify(computer));
+  
+  var card = new UI.Card({
+    title: 'Attempting to wake up...'
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
   card.show();
+  
+  ajax(
+    {
+      url: url + ":" + port,
+      type: 'json',
+      medthod: 'put',
+      headers: computer
+    },
+    function(success) {
+      // Success!
+      console.log('Successfully sent wake up!');
+      card.title('Successfully sent wake up');
+    },
+    function(error) {
+      // Failure!
+      console.log('Failed waking somewhere: ' + error);
+      card.title('Something went wrong :(');
+      card.body(error);
+    }
+  );
 });
+
+menu.show();
