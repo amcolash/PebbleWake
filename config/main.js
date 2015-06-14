1733d9581eec277ffc738f089bd2668a66cf78a4
Andrew McOlash <amcolash@gmail.com>
2015-06-14 14:20:56 -0500

    Added validation to the config form

--- config/main.js
+++ config/main.js
@@ -1,7 +1,12 @@
 function saveOptions() {
   var options = {};
+  var valid = true;
+  var macRegex = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+  var ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+  var domainRegex = /^[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?(\?([-a-zA-Z0-9@:%_\+.~#?&//=]+)|)$/;
+
   //Add all textual values
-  $('textarea, select, [type="hidden"], [type="password"], [type="text"]').each(function () {
+  $('textarea, select, [type="hidden"], [type="password"], [type="text"], [type="number"], [type="range"]').each(function () {
     options[$(this).attr('id')] = $(this).val();
   });
 
@@ -9,7 +14,27 @@ function saveOptions() {
   $('[type="radio"], [type="checkbox"]').each(function () {
     options[$(this).attr('id')] = $(this).is(':checked');
   });
-  return options;
+
+  if (!$('#name').val()) {
+    alert('Invalid Computer Name');
+    valid = false;
+  }
+
+  if (!macRegex.test($('#mac').val())) {
+    alert('Invalid MAC Address');
+    valid = false;
+  }
+
+  if (!ipRegex.test($('#ip').val()) && !domainRegex.test($('#ip').val())) {
+    alert('Invalid IP Address/Domain');
+    valid = false;
+  }
+
+  if (valid) {
+    return options;
+  } else {
+    return null;
+  }
 }
 
 function getQueryParam(variable, defaultValue) {
@@ -38,10 +63,14 @@ $().ready(function () {
     console.log("Submit");
 
     var return_to = getQueryParam('return_to', 'pebblejs://close#');
-    var location = return_to + encodeURIComponent(JSON.stringify(saveOptions()));
-    console.log("Warping to: " + location);
-    console.log(location);
-    document.location = location;
+    var options = saveOptions();
+
+    if (options !== null) {
+      var location = return_to + encodeURIComponent(JSON.stringify(options));
+      console.log("Warping to: " + location);
+      console.log(location);
+      document.location = location;
+    }
   });
 
   //Set form values to whatever is passed in.
